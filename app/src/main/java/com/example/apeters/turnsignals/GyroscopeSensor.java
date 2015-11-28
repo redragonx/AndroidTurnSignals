659b237671d97fa8f3135487d349aaa50cf0cb50
unknown <apeters@5VWLVZ1.ad.anteroresources.com>
2015-11-28 14:47:20 -0700

    Added Brittany's Gyroscope code. Made it into a standalone class. Not currently implemented.

---
+++ app/src/main/java/com/example/apeters/turnsignals/GyroscopeSensor.java
@@ -0,0 +1,64 @@
+package com.example.apeters.turnsignals;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+/**
+ * Created by Brittany on 11/27/2015.
+ */
+public class GyroscopeSensor implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Signals mSignals;
+
+    float quantum = 0.50f,
+            xt = 0.0f,
+            yt = 0.0f,
+            zt = 0.0f;
+
+    public GyroscopeSensor(Context context, Signals signals){
+        mSignals = signals;
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xt += event.values[0]*quantum;
+        yt += event.values[1]*quantum;
+        zt += event.values[2]*quantum;
+
+        if(zt > 3){
+            mSignals.setLeftOn();
+        } else if (zt < -3) {
+            mSignals.setRightOn();
+        } else {
+            mSignals.setLeftOff();
+            mSignals.setRightOff();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public float[] getGyroscopeVals(){
+        float[] f = new float[]{xt, yt, zt};
+        return f;
+    }
+
+    public void setQuantum(float f){
+        quantum = f;
+    }
+
+    public void cancel() {
+        mSensorManager.unregisterListener(this);
+    }
+}
